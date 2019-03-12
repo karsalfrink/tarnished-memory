09f3fca9bcb715524cb6ced64d6edc6784c513d6
Kars Alfrink <kars@leapfrog.nl>
2019-03-12 12:10:17 +0100

    Switch library from list to map

--- tarnished-memory.ino
+++ tarnished-memory.ino
@@ -1,11 +1,16 @@
-#include <QList.h>
+#include <SimpleMap.h>
 
 #define BLE_SCAN_TYPE        0x00   // Passive scanning
 #define BLE_SCAN_INTERVAL    0x0060 // 60 ms
 #define BLE_SCAN_WINDOW      0x0030 // 30 ms
 
-QList<String> devices;
 int deviceCount = 0;
+
+SimpleMap<String, String> *deviceMap = new SimpleMap<String, String>([](String &a, String &b) -> int {
+  if (a == b) return 0;      // a and b are equal
+  else if (a > b) return 1;  // a is bigger than b
+  else return -1;            // a is smaller than b
+});
 
 void reportCallback(advertisementReport_t *report) {
   String peerAddrStr;
@@ -14,11 +19,14 @@ void reportCallback(advertisementReport_t *report) {
     peerAddrStr.concat(String(report->peerAddr[index], HEX));
   }
   
-  if (devices.indexOf(peerAddrStr) > 0) {
+  if (deviceMap->has(peerAddrStr)) {
     // Skip adding a device
   } else {
-    devices.push_front(peerAddrStr);
-    deviceCount = devices.length();
+    String timestamp = String(Time.now());
+    deviceMap->put(peerAddrStr, timestamp);
+
+    deviceCount = deviceMap->size();
+    
     Particle.publish("deviceCount", String(deviceCount));
   }
 }
